02560e231a51255dd1b5204926fe73c3292bf271
Miroslav Mitov <47675969+mnmitov@users.noreply.github.com>
2020-01-29 15:49:21 +0200

    Little code fix

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-import * as elementsModule from './elements.js';
+import * as e from './elements.js';
 import * as stdCalculator from './standart.js';
 import * as clearModule from './clear.js';
 import * as gxCalc from './graphic.js';
 
 // Calc selector
-elementsModule.calcType.addEventListener('click', function (calcSelector) {
-    if (elementsModule.standartCalculator.style.display === 'block' ||
-        elementsModule.graphicCalculator.style.display === 'none') {
-        elementsModule.calcType.textContent = 'Към стандартен калкулатор';
-        elementsModule.standartCalculator.style.display = 'none';
-        elementsModule.graphicCalculator.style.display = 'block';
-        elementsModule.title.textContent = 'Графичен калкулатор';
+e.calcType.addEventListener('click', function (calcSelector) {
+    if (e.standartCalculator.style.display === 'block' ||
+        e.graphicCalculator.style.display === 'none') {
+        e.calcType.textContent = 'Към стандартен калкулатор';
+        e.standartCalculator.style.display = 'none';
+        e.graphicCalculator.style.display = 'block';
+        e.title.textContent = 'Графичен калкулатор';
     } else {
-        elementsModule.calcType.textContent = 'Към графичен калкулатор';
-        elementsModule.standartCalculator.style.display = 'block';
-        elementsModule.graphicCalculator.style.display = 'none';
-        elementsModule.title.textContent = 'Стандартен калкулатор';
+        e.calcType.textContent = 'Към графичен калкулатор';
+        e.standartCalculator.style.display = 'block';
+        e.graphicCalculator.style.display = 'none';
+        e.title.textContent = 'Стандартен калкулатор';
     }
 })
 
 // Clear calculator values and errors
-elementsModule.btnStdClearCalc.addEventListener('click', clearModule.clearCalculator);
+e.btnStdClearCalc.addEventListener('click', clearModule.clearCalculator);
 
 // Standart Calculator
-elementsModule.btnStdCalc.addEventListener('click', stdCalculator.stdCalculator);
+e.btnStdCalc.addEventListener('click', stdCalculator.stdCalculator);
 
 // GraphiX Calculator
 gxCalc.gxCalc();
